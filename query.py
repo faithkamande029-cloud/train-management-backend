4727fb22e790fb9b42c49cceaa0b3bdfc2b99b9b
Faith Kamande <faithkamande029@gmail.com>
2026-07-20 14:45:00 +0300

    Add: notes

--- query.py
+++ query.py
@@ -16,15 +16,20 @@ with app.app_context():
     print("The first user is:", users.first_name, users.last_name, users.status)
 
 
-    # # Apply complex conditional filters.
+    # Apply complex conditional filters.
     users = User.query.filter(User.status == UserStatus.ACTIVE).all()
     print("Active users:")
     for user in users:
         print(user.id, user.last_name, user.first_name, user.status)
 
 
-    # # Apply simple attribute matching.
-    users = User.query.filter_by(status=UserStatus.ACTIVE).all()
-    print("Active users (using filter_by):")
-    for user in users:
-        print(user.id,user.first_name, user.last_name, user.status, user.phone_number)
+    # Use filter_by for simpler equality checks.
+    def get_active_users():
+        users = User.query.filter_by(status=UserStatus.ACTIVE).all()
+        print("Active users (using filter_by):")
+        for user in users:
+            print(user.id,user.first_name, user.last_name, user.status, user.phone_number)
+        return users()
+    
+    get_active_users()
+    
